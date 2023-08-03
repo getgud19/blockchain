f2a5fab728394d8245d04b06c61b63df6ec6e8f4
getgud19 <71492003+getgud19@users.noreply.github.com>
2023-08-03 12:45:36 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+  import './App.css';
+  import Web3 from 'web3'
+
+  function App() {
+    return (
+      <div className="container">
+        <h1>Hello, World!</h1>
+        </div>
+    );
+  }
+
+  export default App;
